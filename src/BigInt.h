e4079460d5d465b058c9653ddee8cfe7f7fd03fa
DSkrzypiec <damian.j.skrzypiec@gmail.com>
2021-06-09 22:24:03 +0200

    Make BigInts "unlimited" size

--- src/BigInt.h
+++ src/BigInt.h
@@ -1,46 +1,19 @@
 #include<iostream>
 #include<string>
+#include<vector>
 
-namespace bigNumCalculator
-{
+namespace bigNumCalculator {
 
-template<int N>
 class BigInt
 {
 private:
-    unsigned char _digits[N];
-    unsigned char _num_of_digits = 0;
+    std::vector<char> _digits;
+    int _num_of_digits = 0;
 
 public:
     BigInt(const std::string& num_str);
+    BigInt(const std::vector<char>& digits);
 
     void print();
 };
-
-template<int N>
-BigInt<N>::BigInt(const std::string& num_str) {
-    auto index = 0;
-
-    for (const auto& ch : num_str) {
-        if (ch >= '0' && ch <= '9') {
-            if (index >= N) {
-                // TODO
-                std::cout << "Digits overflow on " << N << " digits" << std::endl;
-            }
-
-            _digits[index] = ch;
-            _num_of_digits++;
-            index++;
-        }
-    }
-}
-
-template<int N>
-void BigInt<N>::print() {
-    for (auto i = 0; i < _num_of_digits; i++) {
-        std::cout << _digits[i];
-    }
-    std::cout << std::endl;
-}
-
 } // bigNumCalculator namespace
