0c12d42988df74231c36dc9776c6e054c02c6e54
DSkrzypiec <damian.j.skrzypiec@gmail.com>
2021-10-16 14:22:28 +0200

    C# project setup

---
+++ src/bnCalc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bnCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
