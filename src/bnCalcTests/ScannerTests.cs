1a688f70670f870974e0b755d051dd07d3dcf551
DSkrzypiec <damian.j.skrzypiec@gmail.com>
2021-10-20 22:19:37 +0200

    Add tests for Scanner for one parameter expressions

---
+++ src/bnCalcTests/ScannerTests.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using bnCalc;
+
+namespace bnCalcTests
+{
+    public class ScannerTests
+    {
+        [Theory]
+        [MemberData(nameof(OneOperationTestCases))]
+        public void OneOperationTest(string input, List<Token> expectedTokens, List<string> expectedValues)
+        {
+            // Arrange
+            var scanner = new Scanner(input);
+
+            // Act
+            var words = scanner.Scan();
+
+            // Assert
+            Assert.Equal(expectedTokens.Count, words.Count);
+            Assert.Equal(expectedValues.Count, words.Count);
+            Assert.Equal(expectedTokens, words.Select(p => p.Token).ToList());
+            Assert.Equal(expectedValues, words.Select(p => new string(p.Chars.ToArray())).ToList());
+        }
+
+        public static IEnumerable<object[]> OneOperationTestCases()
+        {
+            yield return new object[]
+            {
+                "1+3",
+                new List<Token> { Token.Int, Token.Add, Token.Int },
+                new List<string> { "1", "+", "3"}
+            };
+
+            yield return new object[]
+            {
+                "1   \n         +  3  ",
+                new List<Token> { Token.Int, Token.Add, Token.Int },
+                new List<string> { "1", "+", "3"}
+            };
+
+            yield return new object[]
+            {
+                "1 * 3",
+                new List<Token> { Token.Int, Token.Mul, Token.Int },
+                new List<string> { "1", "*", "3"}
+            };
+
+            yield return new object[]
+            {
+                "3.14 ^ 1.123e10",
+                new List<Token> { Token.Real, Token.Power, Token.Real },
+                new List<string> { "3.14", "^", "1.123e10"}
+            };
+        }
+    }
+}
